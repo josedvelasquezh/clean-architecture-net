878e78e32501e70f204370fb224bf5f906460522
Jose Daniel Velasquez <jvelasquez@thefactoryhka.com>
2023-01-06 20:20:37 -0500

    Se agregan behaviour para validaciones cuando llegan las peticiones

---
+++ CleanArchitecture.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using MediatR;
+
+
+namespace CleanArchitecture.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
+
+                FluentValidation.Results.ValidationResult[] validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                List<FluentValidation.Results.ValidationFailure> failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new Exceptions.ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
