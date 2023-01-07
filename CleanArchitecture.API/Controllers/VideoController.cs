6c8c0bb1bfa6461da7cd2d8d927c9822ef170372
Jose Daniel Velasquez <jvelasquez@thefactoryhka.com>
2023-01-06 21:56:35 -0500

    Se agregan los controller de la capa presentacion

---
+++ CleanArchitecture.API/Controllers/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public VideoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{username}", Name = "GetVideo")]
+        [ProducesResponseType(typeof(IEnumerable<VideosVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<VideosVm>>> GetVideosByUsername(string username)
+        {
+            var query = new GetVideosListQuery(username);
+            var videos = await _mediator.Send(query);
+            return Ok(videos);
+        }
+    }
+}
